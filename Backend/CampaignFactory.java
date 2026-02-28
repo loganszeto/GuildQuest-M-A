2147f9a743fb23aa2837c118c762942ebaa9ecde
decaquestionmarks <nateyheit@gmail.com>
2026-02-27 16:46:54 -0800

    more factories

---
+++ Backend/CampaignFactory.java
@@ -0,0 +1,17 @@
+package backend;
+
+public class CampaignFactory extends FileFactory<Campaign>{
+
+	@Override
+	public void save(Campaign obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Campaign load(String f) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ Backend/RealmFactory.java
@@ -0,0 +1,17 @@
+package backend;
+
+public class RealmFactory extends FileFactory<Realm>{
+
+	@Override
+	public void save(Realm obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Realm load(String f) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
